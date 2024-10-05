68a80b12917eac71d373e26999c95e1425985387
Zubin <kszubin1@gmail.com>
2024-10-05 10:15:32 +0530

    made changes to buttons

--- 11. gui.py
+++ 11. gui.py
@@ -4,13 +4,13 @@ root = tk.Tk()
 root.geometry("400x300")
 root.config(bg="lightblue")
 
-label = tk.Label(root, text="GUI Example")
+label = tk.Label(root, text="GUI Example", width=20, height=2)
 label.pack()
 
 def hello():
     label.config(text="Hello Zubin")
 
-button = tk.Button(root, text="Click me!", command=hello)
-button.pack()
+button = tk.Button(root, text="Click me!", command=hello, width=20, height=2)
+button.pack(pady=20)
 
 root.mainloop()
