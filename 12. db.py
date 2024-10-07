a5da08651e9938fd1868d208c0c45471e7cf6b4c
Zubin <kszubin1@gmail.com>
2024-10-07 09:48:31 +0530

    added select query

--- 12. db.py
+++ 12. db.py
@@ -13,10 +13,14 @@ create = 'CREATE TABLE EMPLOYEE(NAME CHAR(25), AGE INT(3), SALARY INT(10));'
 insert1 = 'INSERT INTO EMPLOYEE VALUES("Zubin", 19, 10000)'
 insert2 = 'INSERT INTO EMPLOYEE VALUES("Nandu", 20, 10000)'
 insert3 = 'INSERT INTO EMPLOYEE VALUES("Ashil", 19, 10000)'
-drop = 'DROP TABLE EMPLOYEE'
+select = 'SELECT * FROM EMPLOYEE'
 
 cursor.execute(create)
 cursor.execute(insert1)
 cursor.execute(insert2)
 cursor.execute(insert3)
-# cursor.execute(drop)
+cursor.execute(select)
+
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
